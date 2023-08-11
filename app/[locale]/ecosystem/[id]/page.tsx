995d6674bd15e574b58742616b8625a7d4d15067
Landay <67254560+landiehan@users.noreply.github.com>
2023-08-11 17:16:13 +0800

    wip: add ecosystem details page

--- app/[locale]/ecosystem/[id]/page.tsx
+++ app/[locale]/ecosystem/[id]/page.tsx
@@ -2,6 +2,9 @@ import Image from 'next/image'
 
 import { ExternalLink } from '@/components/external-link'
 import { Badge } from '@/components/ui/badge'
+import { StarIcon } from '@/components/ui/icons'
+import { getDictionary } from '@/get-dictionary'
+import { Locale } from '@/i18n.config'
 
 import { fetchProject } from '../queries'
 import { getSocialLinkIcon } from '../utils'
@@ -13,13 +16,19 @@ export default async function Page({
 }: {
   params: {
     id: string
+    locale: Locale
   }
 }) {
+  const projectPromise = fetchProject(params.id)
+  const dictionaryPromise = getDictionary(params.locale)
+
   let project
+  let dictionary
+
   try {
-    project = await fetchProject(params.id)
+    ;[project, dictionary] = await Promise.all([projectPromise, dictionaryPromise])
   } catch (error) {
-    return <div className="container">Error data</div>
+    return <div className="container">Failed to load project {params.id}, 🥲</div>
   }
 
   return (
@@ -28,8 +37,8 @@ export default async function Page({
         <BackButton />
       </div>
 
-      <div className="mt-4 lg:mt-[42px] lg:grid lg:grid-cols-[438px,1fr]">
-        <div className="flex flex-col items-center rounded-xl bg-gradient-to-bl from-card-hard to-card px-6 pb-8 pt-6">
+      <div className="mt-4 grid grid-cols-1 items-start gap-5 lg:mt-[42px] lg:grid-cols-[320px,1fr] lg:gap-10 xl:grid-cols-[438px,1fr]">
+        <div className="flex flex-col items-center rounded-xl bg-gradient-to-bl from-card-hard to-card px-6 pb-8 pt-6 lg:sticky lg:top-20">
           <Image
             className="rounded-full"
             src={project.imageUrl}
@@ -44,7 +53,7 @@ export default async function Page({
               <Badge key={category.id}>{category.name}</Badge>
             ))}
           </div>
-          <p className="mt-4 line-clamp-3 text-sm">{project.description}</p>
+          <p className="mt-4 line-clamp-3 break-all text-sm">{project.description}</p>
           <div className="mb-6 mt-8 flex items-center gap-3">
             {project.links.map((link) => (
               <ExternalLink key={link.brand} href={link.url}>
@@ -55,7 +64,67 @@ export default async function Page({
 
           <BookmarkButton />
         </div>
+
+        <div className="grid grid-cols-1 gap-5 lg:gap-10">
+          <div className="aspect-h-5 aspect-w-16 overflow-hidden rounded-xl">
+            <Image
+              src="/ecosystem/banner.png"
+              alt=""
+              className="object-cover"
+              fill
+              unoptimized
+            />
+          </div>
+
+          <section>
+            <h2 className="marker flex items-center">
+              {dictionary.ecosystem.introduction}
+            </h2>
+            <p className="mt-4 lg:mt-6">{project.description}</p>
+          </section>
+
+          <section>
+            <h2 className="marker flex items-center">{dictionary.ecosystem.highlight}</h2>
+            <p className="mt-4 text-soft-foreground lg:mt-6">Nothing here</p>
+          </section>
+
+          <section>
+            <h2 className="marker flex items-center">{dictionary.ecosystem.team}</h2>
+            <p className="mt-4 text-soft-foreground lg:mt-6">Nothing here</p>
+          </section>
+
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:gap-6">
+            <section className="flex items-center gap-4 rounded-xl bg-card p-4 xl:h-20 xl:gap-10">
+              <h2 className="marker flex items-center">{dictionary.ecosystem.stars}</h2>
+              <div className="flex items-center gap-1 xl:gap-3">
+                <StarIcon className="text-border" width={24} height={24} />
+                <StarIcon className="text-border" width={24} height={24} />
+                <StarIcon className="text-border" width={24} height={24} />
+                <StarIcon className="text-border" width={24} height={24} />
+                <StarIcon className="text-border" width={24} height={24} />
+              </div>
+            </section>
+
+            <section className="flex items-center gap-4 rounded-xl bg-card p-4 xl:h-20 xl:gap-10">
+              <h2 className="marker flex items-center">{dictionary.ecosystem.status}</h2>
+            </section>
+
+            <section className="flex items-center gap-4 rounded-xl bg-card p-4 xl:h-20 xl:gap-10">
+              <h2 className="marker flex items-center">{dictionary.ecosystem.stars}</h2>
+            </section>
+
+            <section className="flex items-center gap-4 rounded-xl bg-card p-4 xl:h-20 xl:gap-10">
+              <h2 className="marker flex items-center">{dictionary.ecosystem.audit}</h2>
+            </section>
+
+            <section className="flex items-center gap-4 rounded-xl bg-card p-4 xl:h-20 xl:gap-10">
+              <h2 className="marker flex items-center">{dictionary.ecosystem.token}</h2>
+            </section>
+          </div>
+        </div>
       </div>
+
+      <div className="mt-10 lg:mt-20"></div>
     </div>
   )
 }
