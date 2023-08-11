50584fd18add59311ce7f9f73a4da0e21aca4c43
bleen <bleencn@gmail.com>
2023-08-11 19:16:41 +0800

    feat: homepage news

--- app/[locale]/news.tsx
+++ app/[locale]/news.tsx
@@ -1,5 +1,10 @@
-import { memo } from 'react'
+'use client'
+import clsx from 'clsx'
+import Image from 'next/image'
+import { CSSProperties, memo, useState } from 'react'
+import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react'
 
+import ArrowButton from '@/components/ui/arrow-button'
 import Title from '@/components/ui/Title'
 import { getT } from '@/lib/utils'
 
@@ -10,7 +15,7 @@ const json = {
 interface NewsInfo {
   imgUrl: string
   title: string
-  content: string
+  subtitle: string
   author: string
   authorAvatar: string
   authentication: boolean
@@ -19,9 +24,81 @@ interface NewsInfo {
 
 const newsMocker: NewsInfo[] = [
   {
-    imgUrl: '',
+    imgUrl: '/images/home-news-placeholder.png',
     title: 'How to install Aptos-cli？',
-    content: `Build dApps with exceptional UX, all while relying on Ethereum's unrivaled security, with our high-performance Ethereum layer-2 network built...`,
+    subtitle: `Build dApps with exceptional UX, all while relying on Ethereum's unrivaled security, with our high-performance Ethereum layer-2 network built...`,
+    author: 'WalletConnect',
+    authentication: true,
+    authorAvatar: '/images/home-avatar-placeholder.png',
+    link: '123',
+  },
+  {
+    imgUrl: '/images/home-news-placeholder.png',
+    title: 'How to install Aptos-cli？',
+    subtitle: `with our high-performance Ethereum layer-2 network built...`,
+    author: 'WalletConnect',
+    authentication: true,
+    authorAvatar: '/images/home-avatar-placeholder.png',
+    link: '123',
+  },
+  {
+    imgUrl: '/images/home-news-placeholder.png',
+    title: 'How to install Aptos-cli？',
+    subtitle: `Build dApps with exceptional UX, all while relying on Ethereum's unrivaled security, with our high-performance Ethereum layer-2 network built...`,
+    author: 'WalletConnect',
+    authentication: true,
+    authorAvatar: '/images/home-avatar-placeholder.png',
+    link: '123',
+  },
+  {
+    imgUrl: '/images/home-news-placeholder.png',
+    title: 'How to install Aptos-cli？',
+    subtitle: `Build dApps with exceptional UX, all while relying on Ethereum's unrivaled security, with our high-performance Ethereum layer-2 network built...`,
+    author: 'WalletConnect',
+    authentication: true,
+    authorAvatar: '/images/home-avatar-placeholder.png',
+    link: '123',
+  },
+  {
+    imgUrl: '/images/home-news-placeholder.png',
+    title: 'How to install Aptos-cli？',
+    subtitle: `Build dApps with exceptional UX, all while relying on Ethereum's unrivaled security, with our high-performance Ethereum layer-2 network built...`,
+    author: 'WalletConnect',
+    authentication: true,
+    authorAvatar: '/images/home-avatar-placeholder.png',
+    link: '123',
+  },
+  {
+    imgUrl: '/images/home-news-placeholder.png',
+    title: 'How to install Aptos-cli？',
+    subtitle: `Build dApps with exceptional UX, all while relying on Ethereum's unrivaled security, with our high-performance Ethereum layer-2 network built...`,
+    author: 'WalletConnect',
+    authentication: true,
+    authorAvatar: '/images/home-avatar-placeholder.png',
+    link: '123',
+  },
+  {
+    imgUrl: '/images/home-news-placeholder.png',
+    title: 'How to install Aptos-cli？',
+    subtitle: `Build dApps with exceptional UX, all while relying on Ethereum's unrivaled security, with our high-performance Ethereum layer-2 network built...`,
+    author: 'WalletConnect',
+    authentication: true,
+    authorAvatar: '/images/home-avatar-placeholder.png',
+    link: '123',
+  },
+  {
+    imgUrl: '/images/home-news-placeholder.png',
+    title: 'How to install Aptos-cli？',
+    subtitle: `Build dApps with exceptional UX, all while relying on Ethereum's unrivaled security, with our high-performance Ethereum layer-2 network built...`,
+    author: 'WalletConnect',
+    authentication: true,
+    authorAvatar: '/images/home-avatar-placeholder.png',
+    link: '123',
+  },
+  {
+    imgUrl: '/images/home-news-placeholder.png',
+    title: 'How to install Aptos-cli？',
+    subtitle: `Build dApps with exceptional UX, all while relying on Ethereum's unrivaled security, with our high-performance Ethereum layer-2 network built...`,
     author: 'WalletConnect',
     authentication: true,
     authorAvatar: '/images/home-avatar-placeholder.png',
@@ -29,23 +106,96 @@ const newsMocker: NewsInfo[] = [
   },
 ]
 
+const maskStyle: CSSProperties = {
+  WebkitMask:
+    'linear-gradient(to right,transparent 0%,#fff 20px, #fff calc(100% - 20px), transparent 100%)',
+}
+
 const News = memo(function News() {
   const t = getT(json)
+
+  const [swiper, setSwiper] = useState<SwiperClass>()
+
   return (
     <>
       <Title>{t('title')}</Title>
 
-      <div>
-        {newsMocker.map((news, index) => (
-          <NewsCard key={index} />
-        ))}
+      <div className="relative mt-6 lg:mt-10">
+        <Swiper
+          breakpoints={{
+            0: {
+              slidesPerView: 1.2,
+              centeredSlides: true,
+            },
+            768: {
+              slidesPerView: 2.2,
+            },
+            1024: {
+              slidesPerView: 3.2,
+            },
+            1440: {
+              slidesPerView: 4.2,
+            },
+          }}
+          style={maskStyle}
+          onSwiper={(_swiper) => setSwiper(_swiper)}
+        >
+          {newsMocker.map((news, index) => (
+            <SwiperSlide key={index}>
+              <div className="p-1 md:p-4">
+                <NewsCard news={news} />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        <div
+          className={clsx(
+            'absolute left-0 top-1/2 z-10 hidden -translate-x-2/3',
+            'opacity-50 transition-all hover:opacity-100 lg:block',
+          )}
+          onClick={() => swiper?.slidePrev()}
+        >
+          <ArrowButton direction="tl" />
+        </div>
+        <div
+          className={clsx(
+            'absolute right-0 top-1/2 z-10 hidden translate-x-2/3',
+            'opacity-50 transition-all hover:opacity-100 lg:block',
+          )}
+          onClick={() => swiper?.slideNext()}
+        >
+          <ArrowButton direction="tr" />
+        </div>
       </div>
     </>
   )
 })
 
-const NewsCard = memo(function NewsCard() {
-  return <div>card</div>
+const NewsCard = memo(function NewsCard({ news }: { news: NewsInfo }) {
+  return (
+    <div className="overflow-hidden rounded-3xl">
+      <Image src={news.imgUrl} alt={''} width={420} height={210} />
+
+      <div className="rounded-b-3xl border border-t-0 border-[#DEE1EA] px-3 pb-4 pt-2">
+        <h1 className="mt-2 font-semibold">{news.title}</h1>
+        <div className="mt-2 line-clamp-3 h-[4.5em] text-sm text-black/50">
+          {news.subtitle}
+        </div>
+
+        <div className="mt-4 flex items-center">
+          <Image src={news.authorAvatar} alt={''} width={42} height={42} />
+          <h3 className="flex-1 overflow-hidden text-ellipsis pl-2">
+            <span>{news.author}</span>
+            <span className={clsx({ hidden: !news.authentication })}>icon</span>
+          </h3>
+          <div className="opacity-30 transition-all hover:opacity-100">
+            <ArrowButton type="outline" shape="→" theme="dark" direction="tr" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 })
 
 export default News
