d86a75ab346a4c3bd5c97ade9ef86de1a60c2367
Landay <67254560+landiehan@users.noreply.github.com>
2023-08-14 09:35:26 +0800

    feat: add tab icon and active indicator

--- app/[locale]/ecosystem/featured-projects/tabs.tsx
+++ app/[locale]/ecosystem/featured-projects/tabs.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import Image from 'next/image'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { useTranslations } from 'next-intl'
 import { usePathname } from 'next-intl/client'
+import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
@@ -14,28 +16,49 @@ export function Tabs({ activeTab }: { activeTab: FeaturedProjectsTab }) {
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
+  const [isPending, startTransition] = React.useTransition()
+
   function switchTab(tab: FeaturedProjectsTab) {
     /** @ts-ignore */
     const nextSearchParams = new URLSearchParams(searchParams)
     nextSearchParams.set('tab', tab)
-    router.replace(pathname + '?' + nextSearchParams.toString(), { scroll: false })
+    startTransition(() => {
+      router.replace(pathname + '?' + nextSearchParams.toString(), { scroll: false })
+    })
   }
 
   return (
     <div className="flex items-center gap-[42px]">
       <button
-        className={cn('text-2xl font-semibold', activeTab === 'hot' ? 'text-brand' : '')}
+        className={cn(
+          'relative flex items-center gap-0.5 text-2xl font-semibold duration-200',
+          activeTab === 'hot' ? 'text-brand' : '',
+        )}
         onClick={() => switchTab('hot')}
       >
+        <span
+          className={cn(
+            'absolute right-0 top-full mt-2 h-1 w-[calc(100%-6px)] bg-brand duration-200',
+            activeTab === 'hot' && !isPending ? 'opacity-100' : 'opacity-0',
+          )}
+        />
+        <Image src="/icons/fire.png" alt="" width={32} height={32} />
         {t('hot')}
       </button>
       <button
         className={cn(
-          'text-2xl font-semibold',
+          'relative flex items-center gap-0.5 text-2xl font-semibold duration-200',
           activeTab === 'latest' ? 'text-brand' : '',
         )}
         onClick={() => switchTab('latest')}
       >
+        <span
+          className={cn(
+            'absolute right-0 top-full mt-2 h-1 w-[calc(100%-6px)] bg-brand duration-200',
+            activeTab === 'latest' && !isPending ? 'opacity-100' : 'opacity-0',
+          )}
+        />
+        <Image src="/icons/clover.png" alt="" width={32} height={32} />
         {t('latest')}
       </button>
     </div>
