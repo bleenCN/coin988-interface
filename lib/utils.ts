678522024b9e723fc6da25d818d727d93225dc93
bleen <bleencn@gmail.com>
2023-08-17 18:18:16 +0800

    feat: text replace

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,10 +8,27 @@ export function cn(...inputs: ClassValue[]) {
 /**
  * 新页面暂时替换 i18n 翻译
  */
-export function getT<T>(json: T) {
-  return (key: keyof T) => {
-    return json[key]
+export function getT<T extends { [k: string]: string }>(json: T) {
+  const _json = json
+
+  function t(key: keyof T): string
+  function t(key: keyof T, obj: { [k: string]: string | number | bigint }): string
+  function t(key: keyof T, obj?: { [k: string]: string | number | bigint }) {
+    let res = _json[key] as string
+    if (obj) {
+      const _keys = Object.keys(obj)
+      const regexArr = _keys.map((k) => ({
+        regex: new RegExp(`{\\s*${k}\\s*}`, 'g'),
+        replacer: String(obj[k]),
+      }))
+      for (const { regex, replacer } of regexArr) {
+        res = res.replace(regex, replacer)
+      }
+    }
+    return res
   }
+
+  return t
 }
 
 /**
